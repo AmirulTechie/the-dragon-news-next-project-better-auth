ad9e8e1cb4a9b822e081a3c20115b76166373d80
Md. Amirul Islam <amirulislam.office28@gmail.com>
2026-04-29 00:12:35 +0600

    feat: design login page

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,8 +1,40 @@
+"use client"
+
+import Link from "next/link";
 
 const LoginPage = () => {
+    const handleLoginFunc = (e) =>{
+            e.preventDefault();
+            const email = e.target.email.value;
+            const password = e.target.password.value;
+            console.log("Email:", email, "Password:", password)
+
+    }
+
+
     return (
-        <div>
-            Login
+        <div className="min-h-[80vh] flex justify-center items-center bg-slate-100 mt-10">
+            <div className="p-20 rounded-xl bg-white"> 
+                <h2 className="font-bold text-3xl text-center mb-6">Login your account</h2>
+                <form className="space-y-4" onSubmit={handleLoginFunc}>
+                    <fieldset className="fieldset">
+                        <legend className="fieldset-legend">Email</legend>
+                        <input type="email" className="input" 
+                        name="email"
+                        placeholder="Your email" />
+                        
+                    </fieldset>
+                    <fieldset className="fieldset mb-6">
+                        <legend className="fieldset-legend">Password</legend>
+                        <input type="password" className="input" 
+                        name="password"
+                        placeholder="Your password" />
+                        
+                    </fieldset>
+                    <button className="btn w-full bg-slate-800 text-white">Login</button>
+                </form>
+                <p className="mt-5 text-sm text-center">Dont have an account? <Link href="/register" className="text-blue-500">Register now!</Link></p>
+            </div>
         </div>
     );
 };
--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -1,10 +1,13 @@
 import logo from "@/assets/logo.png"
 import Image from "next/image";
 import { format } from "date-fns";
+import Link from "next/link";
 const Header = () => {
     return (
         <div className="text-center mx-auto py-8 space-y-3">
+            <Link href="/">
             <Image src={logo} alt="Logo" width={400}></Image>
+            </Link>
             <p className="opacity-70">Journalism Without Fear or Favour</p>
             <p className="">{format(new Date(), "EEEE, MMMM dd, yyyy")}</p>
         </div>
