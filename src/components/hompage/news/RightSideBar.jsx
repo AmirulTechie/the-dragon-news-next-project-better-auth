12d4c20b4680f5566b8e49160b5cc381cd531e1e
Md. Amirul Islam <amirulislam.office28@gmail.com>
2026-04-29 23:01:18 +0600

    chore: setup auth client & provider setup with betterauth

--- src/components/hompage/news/RightSideBar.jsx
+++ src/components/hompage/news/RightSideBar.jsx
@@ -1,4 +1,4 @@
-import { FaFacebook, FaGoogle } from "react-icons/fa";
+import { FaGithub, FaGoogle } from "react-icons/fa";
 
 
 const RightSideBar = () => {
@@ -7,7 +7,7 @@ const RightSideBar = () => {
             <h2>Login With</h2>
             <div className="flex flex-col gap-2 mt-5">
                 <button className="btn border-blue-500 text-blue-500 flex justify-center items-center gap-2"><FaGoogle></FaGoogle>Login with Goggle</button>
-                <button className="btn flex justify-center items-center gap-2"><FaFacebook></FaFacebook>Login with Facebook</button>
+                <button className="btn flex justify-center items-center gap-2"><FaGithub></FaGithub>Login with GitHub</button>
             </div>
         </div>
     );
--- src/lib/auth.js
+++ src/lib/auth.js
@@ -2,7 +2,7 @@ import { betterAuth } from "better-auth";
 import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 
-const client = new MongoClient("mongodb+srv://betterAuthDBuserAmirul:RPSWQcYK8tUhZDrb@cluster0.w3nwf9x.mongodb.net/?appName=Cluster0");
+const client = new MongoClient(process.env.MONGO_URI);
 const db = client.db();
 
 export const auth = betterAuth({
@@ -10,4 +10,8 @@ export const auth = betterAuth({
         // Optional: if you don't provide a client, database transactions won't be enabled.
         client
     }),
+
+    emailAndPassword: {
+        enabled: true,
+    },
 });
